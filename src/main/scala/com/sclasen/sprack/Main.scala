fb1424852c769ed62f4799af0773de55f86f6780
Scott Clasen <scott@heroku.com>
2013-06-06 12:35:21 -0700

    allow specifying an akka conf file for the actor system

--- src/main/scala/com/sclasen/sprack/Main.scala
+++ src/main/scala/com/sclasen/sprack/Main.scala
@@ -2,25 +2,25 @@ package com.sclasen.sprack
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
-import akka.pattern._
 import spray.can.Http
 import org.rogach.scallop._
 import concurrent.duration._
 import akka.util.Timeout
-import scala.util.{Failure, Success, Try}
+import akka.pattern._
+import scala.util.{Failure, Success}
+import com.typesafe.config.ConfigFactory
+import java.io.File
 
-object Main extends App  {
+object Main extends App {
 
   val conf = new Conf(args)
-  implicit val system = ActorSystem()
+  implicit val system = createActorSystem(conf)
   implicit val bootTimeout = Timeout(conf.timeout())
-  import concurrent.ExecutionContext.Implicits.global
-
   // the handler actor replies to incoming HttpRequests
   println("creating RackHandler, this can take a while on big apps")
   val handler = system.actorOf(Props(new SprackService(conf.rackfile(), conf.port())), name = "handler")
-
-  (handler ? Ready).onComplete{
+  import concurrent.ExecutionContext.Implicits.global
+  (handler ? Ready).onComplete {
     case Success(Ready) =>
       println("RackHandler Ready binding")
       IO(Http) ! Http.Bind(handler, interface = conf.host(), port = conf.port())
@@ -31,12 +31,20 @@ object Main extends App  {
   }
 
 
+  def createActorSystem(conf: Conf): ActorSystem = {
+    conf.akkafile.get.map {
+      file =>
+        ActorSystem("sprack-system", ConfigFactory.parseFile(new File(file)))
+    }.getOrElse(ActorSystem("sprack-system"))
+  }
+
 }
 
-class Conf(args:Seq[String]) extends ScallopConf(args){
+class Conf(args: Seq[String]) extends ScallopConf(args) {
   version("0.0.0.1")
   val host = opt[String](default = Some("0.0.0.0"))
   val port = opt[Int](default = Some(8080))
   val rackfile = opt[String](default = Some("./config.ru"))
   val timeout = opt[Int](default = Some(30)).map(_ seconds)
+  val akkafile = opt[String]()
 }
